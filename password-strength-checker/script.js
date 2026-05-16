6c171c149d36a61c51b22b6eb79046cc2628a2a7
Hemalatha <hemalathakd07@gmail.com>
2026-05-16 22:02:37 +0530

    Added password strength checker project

--- password-strength-checker/script.js
+++ password-strength-checker/script.js
@@ -1,55 +1,36 @@
 function checkPassword() {
 
     let password = document.getElementById("password").value;
-
     let result = document.getElementById("result");
 
     let score = 0;
 
-    // checking password length
-    if (password.length >= 8) {
-        score++;
-    }
+    // length check
+    if (password.length >= 8) score++;
 
-    // checking uppercase letters
-    if (/[A-Z]/.test(password)) {
-        score++;
-    }
+    // uppercase
+    if (/[A-Z]/.test(password)) score++;
 
-    // checking lowercase letters
-    if (/[a-z]/.test(password)) {
-        score++;
-    }
+    // lowercase
+    if (/[a-z]/.test(password)) score++;
 
-    // checking numbers
-    if (/[0-9]/.test(password)) {
-        score++;
-    }
+    // numbers
+    if (/[0-9]/.test(password)) score++;
 
-    // checking special characters
-    if (/[@$!%*?&]/.test(password)) {
-        score++;
-    }
+    // special characters
+    if (/[@$!%*?&]/.test(password)) score++;
 
-    // displaying result
+    // result logic
     if (score <= 2) {
-
         result.innerHTML = "Weak Password";
         result.style.color = "red";
-
     }
-
     else if (score <= 4) {
-
         result.innerHTML = "Medium Password";
-        result.style.color = "orange";
-
+        result.style.color = "yellow";
     }
-
     else {
-
         result.innerHTML = "Strong Password";
         result.style.color = "lightgreen";
-
     }
 }
